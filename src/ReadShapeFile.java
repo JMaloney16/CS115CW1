a1df756d386714971394c6de42d9275981ad4e4e
Jack Maloney <jackpmaloney1@gmail.com>
2019-02-26 17:30:41 +0000

    Step 1 complete

--- src/ReadShapeFile.java
+++ src/ReadShapeFile.java
@@ -30,17 +30,56 @@ public class ReadShapeFile {
 
 		//read in the shape files and place them on the Queue
 		while(in.hasNextLine()){
+			int diameter;
+			int width;
+			int height;
 			String curLine = in.nextLine();
 			Scanner line = new Scanner(curLine);
-			System.out.println(line.next());
+			String shapeType = line.next();
 
+			int px = line.nextInt();
+			int py = line.nextInt();
+			int vx = line.nextInt();
+			int vy = line.nextInt();
+			boolean filled = line.nextBoolean();
+
+			if (shapeType.equals("circle")){
+				diameter = line.nextInt();
+			} else if (shapeType.equals("oval")){
+				width = line.nextInt();
+				height = line.nextInt();
+			}
+
+			int r = line.nextInt();
+			int g = line.nextInt();
+			int b = line.nextInt();
+			int insertionTime = line.nextInt();
 			line.close();
+
+			if(shapeType.equals("circle")){
+				shapeQueue.enqueue(createCircle(insertionTime, px, py, vx, vy, diameter, r, g, b, filled));
+			}else if(shapeType.equals("oval")){
+				shapeQueue.enqueue(createOval(insertionTime, px, py, vx, vy, width, height, r, g, b, filled));
+			}
 		}
+		in.close();
 		//Right now, returning an empty Queue.  You need to change this.
 		return shapeQueue;
 	}
 
+	private static Circle createCircle(int insertionTime, int px, int py, int vx, int vy, int diameter,
+									   int r, int g, int b, boolean filled){
 
+//		System.out.println(insertionTime + " " + " " + px +  " " + py + " " + vx + " " + vy + " " + diameter
+//				+ " " + Color.rgb(r,g,b) + " " + filled);
+		return new Circle(insertionTime, px, py, vx, vy, diameter, Color.rgb(r,g,b), filled);
+	}
+	private static Oval createOval(int insertionTime, int px, int py, int vx, int vy, int width, int height,
+								   int r, int g, int b, boolean filled){
+//		System.out.println(insertionTime + " " + " " + px +  " " + py + " " + vx + " " + vy + " " + width + " " +
+//				height + " " + Color.rgb(r,g,b) + " " + filled);
+		return new Oval(insertionTime, px, py, vx, vy, width, height, Color.rgb(r,g,b), filled);
+	}
 
 
 
@@ -61,11 +100,10 @@ public class ReadShapeFile {
 			in = new Scanner(file);
 		}
 		catch (FileNotFoundException e){
-			System.out.println("FIle does not exist.");
+			System.out.println("File does not exist.");
 			System.exit(0);
 		}
 
 	    return ReadShapeFile.readDataFile(in);
-	    
 	}
 }
